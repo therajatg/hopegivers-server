a03d32e2fa47e2e58c0a1121c083bd8993096959
Rajat Gupta <dguptarajat@gmail.com>
2023-07-05 19:11:03 +0530

    image optimization

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -30,14 +30,19 @@ const postImage = async (req, res) => {
     const file = req.file;
 
     // const buffer = await sharp(file.buffer)
-    //   .resize({ height: 1920, width: 1080, fit: "contain" })
+    //   .resize({ height: 100, width: null, fit: "contain" })
     //   .toBuffer();
+
+    const buffer = await sharp(file.buffer)
+      .resize({ height: 100, width: null, fit: "contain" })
+      .toFormat("jpeg", { mozjpeg: true });
 
     const params = {
       Bucket: bucketName,
       // Key: req.file.originalname,
       Key: randomImageName,
-      Body: file.buffer,
+      // Body: file.buffer,
+      Body: buffer,
       ContentType: req.file.mimetype,
     };
 
